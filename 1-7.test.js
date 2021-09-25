509cc9e4ebcba0ade96642f755f4acc66c7e5a84
Anna Roos <anna.flytstrom@gmail.com>
2021-09-25 12:30:27 +0200

    reorganized test files

--- 1-7.test.js
+++ 1-7.test.js
@@ -19,7 +19,7 @@ const {
   dominantDirection,
 } = require('./chapter5.js');
 const { Vec, Group } = require('./chapter6.js');
-const { PGroup } = require('./chapter7.js');
+
 
 //Looping a triangle
 
@@ -245,27 +245,3 @@ describe('Groups', () => {
 });
  */
 
-//Persistent Groups
-
-describe('Persistent Groups', () => {
-  test(
-    'Should create a class that works as a Set and creates a' +
-      'new Set for every addition or deletion',
-    () => {
-      let a = PGroup.empty.add('a');
-      let ab = a.add('b');
-      let b = ab.delete('a');
-      expect(b.has('b')).toBe(true);
-      expect(a.has('b')).toBe(false);
-      expect(b.has('a')).toBe(false);
-    }
-  );
-});
-
-//Retry
-
-describe('Retry', () => {
-  test('Create a function that handles exceptions and keeps retrying until a call succeeds', () => {
-    expect(reliableMultiply(8, 8)).toEqual(64);
-  });
-});
