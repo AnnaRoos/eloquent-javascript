47c667309be9b0b6ffd83297f2881241d9f64800
Anna Roos <anna.flytstrom@gmail.com>
2021-10-18 11:47:51 +0200

    starting chapter 17

---
+++ chapters/chapter17.js
@@ -0,0 +1,12 @@
+//Shapes
+let cx = document.querySelector('canvas').getContext('2d');
+
+//1. A trapezoid (a rectangle that is wider on one side)
+
+//2. A red diamond (a rectangle rotated 45 degrees or ¼π radians)
+
+//3. A zigzagging line
+
+//4. A spiral made up of 100 straight line segments
+
+//5. A yellow star
