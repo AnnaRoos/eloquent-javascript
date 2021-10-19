406d51a4e2da2f0dafbca4f35413b85fe1f0901e
Anna Roos <anna.flytstrom@gmail.com>
2021-10-19 17:10:49 +0200

    done with shapes chapter 17

--- chapters/chapter17.js
+++ chapters/chapter17.js
@@ -2,17 +2,20 @@
 let cx = document.querySelector('canvas').getContext('2d');
 
 //1. A trapezoid (a rectangle that is wider on one side)
-const trapezoid = (context, startX, startY, upperSide, bottomSide, height) => {
-  const difference =
-    upperSide > bottomSide
-      ? (upperSide - bottomSide) * Math.sign(-1)
-      : bottomSide - upperSide;
+const trapezoid = (
+  context,
+  centerX,
+  centerY,
+  upperSide,
+  bottomSide,
+  height
+) => {
   context.beginPath();
-  context.moveTo(startX, startY);
-  context.lineTo(startX + upperSide, startY);
-  context.lineTo(startX + upperSide + difference / 2, startY + height);
-  context.lineTo(startX - difference / 2, startY + height);
-  context.lineTo(startX, startY);
+  context.moveTo(centerX - upperSide / 2, centerY - height / 2);
+  context.lineTo(centerX + upperSide / 2, centerY - height / 2);
+  context.lineTo(centerX + bottomSide / 2, centerY + height / 2);
+  context.lineTo(centerX - bottomSide / 2, centerY + height / 2);
+  context.closePath();
   context.stroke();
 };
 
@@ -29,8 +32,8 @@ const diamond = (context, centerX, centerY, side) => {
   context.restore();
 };
 
-diamond(cx, 50, 50, 50);
-trapezoid(cx, 50, 50, 30, 70, 60);
+diamond(cx, 200, 150, 50);
+trapezoid(cx, 200, 150, 30, 70, 60);
 
 //3. A zigzagging line
 const zigzag = (context, startX, startY, width, height, lines) => {
@@ -55,15 +58,31 @@ zigzag(cx, 300, 60, 50, 100, 9);
 //Got some help from Stack overflow...
 const spiral = (context, centerX, centerY, lineLength) => {
   context.moveTo(centerX, centerY);
-    context.beginPath();
-    for (i = 0; i < 100; i++) {
-      let angle = lineLength * i;
-      x = centerX + angle * Math.cos(angle);
-      y = centerY + angle * Math.sin(angle);
-      context.lineTo(x, y);
-    }
+  context.beginPath();
+  for (i = 0; i < 100; i++) {
+    let angle = lineLength * i;
+    x = centerX + angle * Math.cos(angle);
+    y = centerY + angle * Math.sin(angle);
+    context.lineTo(x, y);
+  }
   context.stroke();
 };
 spiral(cx, 450, 100, 0.9);
 
 //5. A yellow star
+const star = (context, centerX, centerY, size, beams) => {
+  const angle = (Math.PI * 2) / beams;
+  context.beginPath();
+  context.moveTo(centerX + size, centerY);
+  for (i = 0; i <= beams; i++) {
+    x = centerX + size * Math.cos(angle * i);
+    y = centerY + size * Math.sin(angle * i);
+    context.quadraticCurveTo(centerX, centerY, x, y);
+  }
+  context.strokeStyle = '#F8981D';
+  context.stroke();
+  context.fillStyle = '#F8981D';
+  context.fill();
+};
+
+star(cx, 600, 300, 100, 10);
