bed4346b82e806f9ea7873e17739c9e98b2362fc
Anna Roos <anna.flytstrom@gmail.com>
2021-10-20 10:31:19 +0200

    2/4 on chapter 17

--- chapters/chapter17.js
+++ chapters/chapter17.js
@@ -85,4 +85,47 @@ const star = (context, centerX, centerY, size, beams) => {
   context.fill();
 };
 
-star(cx, 600, 300, 100, 10);
+star(cx, 600, 300, 100, 8);
+
+//The pie chart
+const results = [
+  { name: 'Satisfied', count: 100, color: 'lightblue' },
+  { name: 'Neutral', count: 53, color: 'lightgreen' },
+  { name: 'Unsatisfied', count: 510, color: 'pink' },
+  { name: 'No comment', count: 75, color: 'silver' },
+];
+let total = results.reduce((sum, { count }) => sum + count, 0);
+let currentAngle = -0.5 * Math.PI;
+let centerX = 300,
+  centerY = 400;
+let radius = 100;
+
+for (let result of results) {
+  let sliceAngle = (result.count / total) * 2 * Math.PI;
+  let textPosition = currentAngle + sliceAngle / 2;
+  let offsetX = 0;
+  let offsetY = 0;
+  console.log(textPosition);
+  
+  cx.beginPath();
+  cx.arc(centerX, centerY, radius, currentAngle, currentAngle + sliceAngle);
+
+  cx.lineTo(centerX, centerY);
+  cx.fillStyle = result.color;
+  cx.fill();
+  cx.font = '14px Georgia';
+  cx.fillStyle = 'black';
+  textPosition > Math.PI / 2
+    ? (offsetX = -90)
+    : (offsetX = 20);
+  textPosition > 4 || textPosition < -1
+    ? (offsetY = -20)
+    : offsetY;
+  textPosition > 1 && textPosition < 2 ? offsetY = 20 : offsetY;
+  cx.fillText(
+    result.name,
+    centerX + offsetX + radius * Math.cos(textPosition),
+    centerY + offsetY + radius * Math.sin(textPosition)
+  );
+  currentAngle += sliceAngle;
+}
